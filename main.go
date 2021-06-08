da211ea4a1e3ef8c462dcb2244891e278a31cae2
ak1484 <ankitsaharan1484@gmail.com>
2021-06-08 17:03:37 +0530

    Console Input (Bufio Scanner) & Type Conversion

--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 func main() {
-	// fmt.Printf("%T",10)
-	// fmt.Printf("Hello %T %v",10,10)
-	// fmt.Printf("Hello %t",10)
-	// fmt.Printf("Hello %t",true)
-	// fmt.Printf("Number : %b",3322)
-	// fmt.Printf("Number : %o",3322)
-	//%% for % sign 
-	//%e scintific notation and f standerd ,g very exponent
-	//%s  to string and q also include "" 
-	//width and percision in f for default width and precision
-	//9f for 9 width and 0.2f for for 2 precision and 9.2 is for 9 width and 2 precision
-	//%09d is for 0's up to 0 preciding and last number at the end
-	// \n new line and \t for tab or space of some spaces
-	fmt.Println("Printing to Console & fmt")
+	scanner:=bufio.NewScanner(os.Stdin)
+	fmt.Printf("Type the year you were born: ")
+	scanner.Scan()
+	input,_:= strconv.ParseInt(scanner.Text(),10,64)
+	//if you input a string then the input is 0 
+	fmt.Printf("You will be %d years old at the end of the year",2021-input)
 }
