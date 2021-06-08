02cb0609b4f1e71a0804e1a19641aef99e3c47e8
ak1484 <ankitsaharan1484@gmail.com>
2021-06-08 17:32:30 +0530

    Chained Conditionals (AND, OR, NOT)

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	// "bufio"
 	"fmt"
 	// "os"
 	// "strconv"
 )
 func main() {
-	fmt.Println("Conditions & Boolean Expressions")
-	//<,>,>=,<=,!=,== are all condition
-	x:=5
-	y:=6.4
-	ans:=float64(x)!=y
-	fmt.Printf("%t",ans)
-	a:="tim"
-	b:="tim"
-	c:="Tim"
-	fmt.Printf(" %t",a==b)
-	fmt.Printf(" %t",a==c)
+	fmt.Println("Chained Conditionals (AND, OR, NOT)")
+	// || or, && and , ! not start from left to right evaluating
+	val:=false && (false || true)|| ! (8>7)
+	fmt.Printf("%t",val)
 }
