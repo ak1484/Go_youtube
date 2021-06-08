176a6e56f9a2dbb2e8ee1024b88604b68c601ffd
ak1484 <ankitsaharan1484@gmail.com>
2021-06-09 00:42:47 +0530

    func as a variable

--- main.go
+++ main.go
@@ -5,16 +5,12 @@ import (
 	// "os"
 	// "strconv"
 ) 
-func test(x,y int)(z1 int,z2 int){
-	defer fmt.Println("clean up the extra memory use")
-	z1=x+y
-	z2=y-x
-	fmt.Println("start")
-	return 
-}
+
 func main() {
-	fmt.Println("Functions")
-	//Functions
-	add,sub:=test(4,9)
-	fmt.Println("Sum:",add,sub)
+	fmt.Println("Advanced Function Concepts & Function Closures")
+	//Advanced Function Concepts & Function Closures
+	test:=func(){
+		fmt.Println("test function")
+	}
+	test()
 }
