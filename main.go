4481e261f0bfd2e91ffe645dbddd4f7092497d26
ak1484 <ankitsaharan1484@gmail.com>
2021-06-08 23:05:28 +0530

    If, Else If, Else

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 	// "strconv"
 )
 func main() {
-	fmt.Println("Chained Conditionals (AND, OR, NOT)")
-	// || or, && and , ! not start from left to right evaluating
-	val:=false && (false || true)|| ! (8>7)
-	fmt.Printf("%t",val)
+	fmt.Println("If, Else If, Else ")
+	//for if else condition
+	age:=14
+	if age>=18{
+		fmt.Printf("You can drive")
+	}else if age>=14{
+		fmt.Printf("You can drive but with your parents")
+	}else{
+		println("you can not drive")
+	}
 }
