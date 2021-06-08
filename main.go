aab88e0c652078e984e7574a6cbe272445749c7b
ak1484 <ankitsaharan1484@gmail.com>
2021-06-08 23:23:35 +0530

    Switch Statement

--- main.go
+++ main.go
@@ -6,15 +6,23 @@ import (
 	// "strconv"
 )
 func main() {
-	fmt.Println("For Loops & While Loops")
-	//for loops and while loop
-	// x:=0
-	// for x<5{
-    //   fmt.Println(x)
-	//   x++
-	// }
-	for x:=0;x<9;x+=2{
-		fmt.Println(x)
+	fmt.Println("Switch Statement")
+	//switch statement
+    ans:=10
+	for i:=1;i<ans;i++{
+		rem:=ans%i
+		switch rem{
+		case 1:{
+           fmt.Printf("%d",rem)
+		}
+	    case 2:{
+		   fmt.Printf("%d",rem)
+	    }
+	    default:{
+			fmt.Printf("%d",rem)
+		}
+		}
+		fmt.Printf("\n")
 	}
-	//break up to the end and continue up to the for loop
+	
 }
