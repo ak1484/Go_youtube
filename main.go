4bc68dc93c344e95da956997d662fb57312ef542
ak1484 <ankitsaharan1484@gmail.com>
2021-06-08 11:10:04 +0530

    Assignment Expression & Implicit vs Explicit

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 func main() {
-	var number =260
-	number=number+5
-	var name string="Ankit"
-	fmt.Println(name,number)
+	// var number =260000.98
+	// n:=6
+	// n:="snkit"
+	// n:=true
+	// fmt.Printf("%T",n)
+	// var n uint64
+	// fmt.Println(n)
+	// var bl bool
+	// fmt.Println(bl)
+	fmt.Println("Assignment Expression & Implicit vs Explicit")
 }
