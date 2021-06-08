d6c89ee3443079933df109dc54017a5ae9d814b3
ak1484 <ankitsaharan1484@gmail.com>
2021-06-08 12:11:59 +0530

    Printing to Console & fmt

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import "fmt"
 func main() {
-	// var number =260000.98
-	// n:=6
-	// n:="snkit"
-	// n:=true
-	// fmt.Printf("%T",n)
-	// var n uint64
-	// fmt.Println(n)
-	// var bl bool
-	// fmt.Println(bl)
-	fmt.Println("Assignment Expression & Implicit vs Explicit")
+	// fmt.Printf("%T",10)
+	// fmt.Printf("Hello %T %v",10,10)
+	// fmt.Printf("Hello %t",10)
+	// fmt.Printf("Hello %t",true)
+	// fmt.Printf("Number : %b",3322)
+	// fmt.Printf("Number : %o",3322)
+	//%% for % sign 
+	//%e scintific notation and f standerd ,g very exponent
+	//%s  to string and q also include "" 
+	//width and percision in f for default width and precision
+	//9f for 9 width and 0.2f for for 2 precision and 9.2 is for 9 width and 2 precision
+	//%09d is for 0's up to 0 preciding and last number at the end
+	// \n new line and \t for tab or space of some spaces
+	fmt.Println("Printing to Console & fmt")
 }
