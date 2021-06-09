b5c938e7b0eb8b25effb4d988f69ed626cb2d62f
ak1484 <ankitsaharan1484@gmail.com>
2021-06-09 15:48:44 +0530

    Mutable & Immutable Data Types

--- main.go
+++ main.go
@@ -7,10 +7,25 @@ import (
 ) 
 
 func main() {
-	fmt.Println("Advanced Function Concepts & Function Closures")
-	//Advanced Function Concepts & Function Closures
-	test:=func(){
-		fmt.Println("test function")
-	}
-	test()
+	fmt.Println("Mutable & Immutable Data Types")
+	//Mutable & Immutable Data Types
+	// x:=5
+	// y:=x
+	// y=7
+	// fmt.Println(x,y)
+	// x:=[]int{3,5,6}
+	// y:=x
+	// y[0]=100
+	// fmt.Println(x,y)
+	//MAPS
+	// x:=map[string]int{"hello":3}
+    // y:=x
+	// y["y"]=100
+	// x["x"]=7
+	// fmt.Println(x,y)
+	//Array
+	x:=[2]int{4,6}
+	y:=x
+	y[0]=8
+	fmt.Println(x,y)
 }
