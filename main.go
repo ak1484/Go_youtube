b9edade55a87c78dbd1027ac932431e7c7561f6b
ak1484 <ankitsaharan1484@gmail.com>
2021-06-09 16:38:58 +0530

    Pointers & Derefrence Operator (& and *)

--- main.go
+++ main.go
@@ -4,28 +4,34 @@ import (
 	"fmt"
 	// "os"
 	// "strconv"
-) 
+)
 
+// func strchange(str *string){
+// 	*str="changed"
+// }
+// func str1change(str1 string){
+// 	str1="changed"
+// }
 func main() {
-	fmt.Println("Mutable & Immutable Data Types")
-	//Mutable & Immutable Data Types
-	// x:=5
-	// y:=x
-	// y=7
+	fmt.Println("Pointers & Derefrence Operator (& and *)")
+	//Pointers & Derefrence Operator (& and *)
+	// x:=8
+	// fmt.Println(&x)
+	// x:=7
+	// y:=&x
 	// fmt.Println(x,y)
-	// x:=[]int{3,5,6}
-	// y:=x
-	// y[0]=100
-	// fmt.Println(x,y)
-	//MAPS
-	// x:=map[string]int{"hello":3}
-    // y:=x
-	// y["y"]=100
-	// x["x"]=7
-	// fmt.Println(x,y)
-	//Array
-	x:=[2]int{4,6}
-	y:=x
-	y[0]=8
-	fmt.Println(x,y)
+	// *y=9
+	// fmt.Println(x)
+	// x:="no change"
+	// y:="no change"
+	// strchange(&x)
+	// str1change(y)
+	// fmt.Println(x)
+	// fmt.Println(y)
+    // x:=9
+	// y:=&x
+	// fmt.Println(y)
+	x:="no change"
+	var y *string=&x
+	fmt.Println(&y,"",y,"",*y)
 }
